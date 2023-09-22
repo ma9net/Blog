da6a32d214f24cdd45a57d4e2a1e9012b1cdf322
ma9net <dehhadi7@gmail.com>
2023-09-22 13:28:01 +0330

    Initial commit

---
+++ urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from . import views
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
+from .feeds import LatestPostsFeed
+
+
+app_name = 'blog'
+
+sitemaps = {
+    'posts': PostSitemap,
+}
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
+    # path('', views.PostListView.as_view(), name='post_list'),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('search/', views.post_search, name='post_search'),
+]
